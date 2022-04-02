7d1b7c595d6af5919155883f0c6de235c8cefe95
Torbjörn Ahlbom <ahlbom.tog@gmail.com>
2022-04-02 18:39:47 +0200

    finished header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,9 +8,19 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavor" />
       <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: "2rem 0" }}></p>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit Tellus Lortis Sed Senectus Vivamus Molestie. Condimentum Volupat
+        Marbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet
+        Tellus.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
-    <div className="app__wrapper_img"></div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
